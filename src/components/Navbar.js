70254bf50e41140102855d3281c9ca1d4ca3be9d
Poojan Shah <poojan23@gmail.com>
2024-12-30 11:19:55 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import NavLink from "./NavLink";
 
-import { Bars3Icon, XmarkIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
@@ -62,7 +62,7 @@ const Navbar = () => {
               onClick={() => setNavbarOpen(false)}
               className="flex items-center px-3 py-2 border rounded broder-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
-              <XmarkIcon className="h-5 w-5" />
+              <XMarkIcon className="h-5 w-5" />
             </button>
           )}
         </div>
